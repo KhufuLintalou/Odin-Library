c6c59fee126d272bd538f7239dc6e63efc92fcb8
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2024-11-04 17:42:44 +0700

    Added a function that displays all the books.

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
 const myLibrary = [];
 
-function Book(title, author, pages, hasRead) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.hasRead = hasRead;
+    this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, hasRead) {
-    const book = new Book(title, author, pages, hasRead);
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
 
-addBookToLibrary("book#1", "author#1", 123, true);
-addBookToLibrary("book#2", "author#2", 123, false);
-addBookToLibrary("book#3", "author#3", 123, false);
-addBookToLibrary("book#4", "author#4", 123, true);
+addBookToLibrary("book#1", "author#1", 345, "Read");
+addBookToLibrary("book#2", "author#2", 210, "Haven't Read Yet");
+addBookToLibrary("book#3", "author#3", 120, "Read");
+addBookToLibrary("book#4", "author#4", 520, "Haven't Read Yet");
+
+const cardContainer = document.querySelector(".card-container");
+
+function displayAllBook() {
+  myLibrary.forEach((e) => {
+    const card = document.createElement("div");
+    const cardTop = document.createElement("div");
+    const cardBottom = document.createElement("div");
+    const title = document.createElement("div");
+    const author = document.createElement("div");
+    const pages = document.createElement("div");
+    const read = document.createElement("div");
+      
+    card.className = "card";
+    cardTop.className = "card-top";
+    cardBottom.className = "card-bottom";
+    title.className = "title";
+    author.className = "author";
+    pages.className = "pages";
+    read.className = "read";
+      
+      
+    title.textContent = e.title;
+    author.textContent = e.author;
+    pages.textContent = e.pages;
+    read.textContent = e.read;
+      
+    cardContainer.appendChild(card);
+    card.appendChild(cardTop);
+    card.appendChild(cardBottom);
+    cardTop.appendChild(title);
+    cardTop.appendChild(author);
+    cardBottom.appendChild(pages);
+    cardBottom.appendChild(read);
+  })
+}
+
+displayAllBook();
